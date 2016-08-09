e392cefa0945cb185a91dd7e7deaa7b9b0bc5ad4
pjlangley <langleyjpeter@gmail.com>
2016-08-09 10:54:24 +0100

    Removed setTimeout from fetchLocationAsync thunk routine

--- src/modules/thunks.js
+++ src/modules/thunks.js
@@ -7,31 +7,29 @@ export function fetchLocationAsync(locationId) {
 
         dispatch(fetchLocation());
 
-        setTimeout(function() {
-            var result = fetch(locationId);
+        var result = fetch(locationId);
 
-            result.then(function onResolve(data, textStatus, jqXHR) {
-                var location = data.SiteRep.DV.Location;
-                var stats = location.Period[0].Rep[0];
+        result.then(function onResolve(data, textStatus, jqXHR) {
+            var location = data.SiteRep.DV.Location;
+            var stats = location.Period[0].Rep[0];
 
-                var payload = {
-                    id: location.i,
-                    name: location.name,
-                    lat: location.lat,
-                    lon: location.lon,
-                    country: location.country,
-                    continent: location.continent,
-                    temperature: stats.T + '&deg;C',
-                    windSpeed: stats.S + 'mph',
-                    windDirection: stats.D
-                };
+            var payload = {
+                id: location.i,
+                name: location.name,
+                lat: location.lat,
+                lon: location.lon,
+                country: location.country,
+                continent: location.continent,
+                temperature: stats.T + '&deg;C',
+                windSpeed: stats.S + 'mph',
+                windDirection: stats.D
+            };
 
-                return dispatch(receiveLocation(payload));
+            return dispatch(receiveLocation(payload));
 
-            }, function onFail(jqXHR, textStatus, errorThrown) {
-                return dispatch(receiveLocation(new Error()));
-            });
-        }, 1000);
+        }, function onFail(jqXHR, textStatus, errorThrown) {
+            return dispatch(receiveLocation(new Error()));
+        });
     }
 }
 
